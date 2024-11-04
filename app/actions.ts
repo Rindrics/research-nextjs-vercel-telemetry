fce7cbbedaf9540e9a358669bf67886d94d2b46c
Akira Hayashi <rindrics@gmail.com>
2024-11-05 07:43:04 +0900

    Wait for 10s

--- app/actions.ts
+++ app/actions.ts
@@ -53,7 +53,7 @@ async function flushTelemetry() {
     await Promise.all([
       metricReader.forceFlush(),
       loggerProvider.forceFlush(),
-      new Promise(resolve => setTimeout(resolve, 1000)), // wait for exporting
+      new Promise(resolve => setTimeout(resolve, 10000)), // wait for exporting
     ]);
     log(SeverityNumber.INFO, 'Logs after me will be visible at next function call because we are outside of Promise');
     log(SeverityNumber.INFO, 'Metrics and logs flushed successfully');
